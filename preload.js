e198f72bc4e3d38b62adca3756806defb96ccd39
smallyunet <499503799@qq.com>
2020-03-18 12:29:18 +0800

    m

--- preload.js
+++ preload.js
@@ -3,28 +3,24 @@ var fs = require('fs');
 let loadConfig = function () {
   fs.readFile('/home/smallyu/apps/trojan/config.json', (err, data) => {
     // todo err handle
-
     parseFile(data)
   })
 }
 
 let parseFile = function (data) {
   let config = JSON.parse(data)
-  console.log(config)
+  // console.log(config)
 
-  let properties = {
-    'remote_addr': {
-      get: function() {
-        console.log('get')
-      },
-      set: function() {
-        console.log('set')
-      }
-    }
+  let configEle = document.getElementById('config')
+  configEle.innerHTML = `服务器地址：${config.remote_addr}`
+
+  let remoteAddrEle = document.getElementById("remote_addr")
+  remoteAddrEle.value = config.remote_addr
+  let remoteAddrChange = function() {
+    config.remote_addr = remoteAddrEle.value
+    configEle.innerHTML = `服务器地址：${config.remote_addr}`
   }
-  Object.defineProperties(config, properties)
-
-  config.remote_addr = '123'
+  remoteAddrEle.addEventListener('keyup', remoteAddrChange)
 }
 
 window.addEventListener('DOMContentLoaded', () => {
